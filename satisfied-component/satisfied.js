08e6dce7d4ba02e1a6e33bde6ac00be40b684c49
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-10-27 19:05:57 +0200

    2 files modified

--- satisfied-component/satisfied.js
+++ satisfied-component/satisfied.js
@@ -3,8 +3,47 @@ export class SatisfiedComponent {
   async getComponent() {
     console.log('satisfied works');
     const html = await (await fetch('./satisfied-component/satisfied.html')).text();
-    // await this.onInit();
+    this.onInit();
 
     return html;
   }
+
+  onInit() {
+    // Timeout necessario altrimenti i componenti non vengono trovato sul DOM
+    setTimeout(() => {      
+      this.listenGoToGoogle();
+      this.listenGoToTestimonials();
+    }, 0)
+  }
+
+  // #region listeners
+
+  listenGoToGoogle() {
+    const component = document.getElementById('google-button');
+    if(component == null) {
+      console.err('Component not yet in DOM');
+      return;
+    }
+    component.addEventListener('click', ()=> {
+      console.log('fire go to google');
+      window.open('https://www.google.com/search?hl=it-IT&gl=it&q=Zen+Home+Shop,+Via+Euclide,+7,+21050+Lonate+Ceppino+VA&ludocid=17644751623097458454&lsig=AB86z5Wdr3MGc69N2WM60BQu03Ny#lrd=0x478688a63893ffff:0xf4dec01d84b89f16,3')
+    });
+  }
+  
+  listenGoToTestimonials() {
+    const component = document.getElementById('video-testimonial-button');
+    if(component == null) {
+      console.err('Component not yet in DOM');
+      return;
+    }
+    component.addEventListener('click', ()=> {
+      console.log('**YET TO DO**');
+    });
+  }
+
+  // #endregion
 }
+
+
+
+
