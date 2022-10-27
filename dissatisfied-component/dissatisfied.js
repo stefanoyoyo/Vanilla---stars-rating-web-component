4aaa6dc1ce24fb6d5d35102e5be44152e17ee075
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-10-27 19:17:27 +0200

    1 file modified

--- dissatisfied-component/dissatisfied.js
+++ dissatisfied-component/dissatisfied.js
@@ -19,8 +19,28 @@ export class DissatisfiedComponent {
   listenEmailSend() {
     const component = document.getElementById('email-send');
     component.addEventListener('click', () => {
-      console.log('email send clicked')
+      console.log('email send clicked');
+      const data = this.getFormData();
+      console.log(data)
     });
   }
+
+  // #region get form data 
+
+  getFormData() {
+    const name = document.getElementById('name')?.innerHtml;
+    const email = document.getElementById('email')?.innerHtml;
+    const telehone = document.getElementById('telephone')?.innerHtml;
+    const review = document.getElementById('review')?.innerHtml;
+
+    return {
+      name: name,
+      email: email,
+      telehone: telehone,
+      review: review,
+    };
+  }
+
+  // #endregion 
 
 }
