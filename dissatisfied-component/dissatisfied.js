612d921223bacdaab63f51938c6d86447325899c
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-10-27 19:11:35 +0200

    2 files modified

--- dissatisfied-component/dissatisfied.js
+++ dissatisfied-component/dissatisfied.js
@@ -3,8 +3,25 @@ export class DissatisfiedComponent {
   async getComponent() {
     console.log('dissatisfied works');
     const html = await (await fetch('./dissatisfied-component/dissatisfied.html')).text();
-    // await this.onInit();
+    this.onInit();
 
     return html;
   }
+
+  
+  onInit() {
+    // Timeout necessario altrimenti i componenti non vengono trovato sul DOM
+    setTimeout(() => {      
+      this.listenEmailSend();
+    }, 0);
+  }
+
+  listenEmailSend() {
+    const component = document.getElementById('email-send');
+    component.addEventListener('click', () => {
+      /**CAPIRE PERCHè NON FUNZIONA IL CLICK */
+      console.log('email send clicked')
+    });
+  }
+
 }
