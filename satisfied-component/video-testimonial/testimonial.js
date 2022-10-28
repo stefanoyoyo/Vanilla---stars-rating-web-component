7956453d01599d1ad7b5f6a93cba362f3358dfb7
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-10-28 21:28:54 +0200

    2 files modified

--- satisfied-component/video-testimonial/testimonial.js
+++ satisfied-component/video-testimonial/testimonial.js
@@ -3,8 +3,24 @@ export class TestimonialComponent {
     /**Method to get the this component html's code */
     async getComponent() {
       const html = await (await fetch('./satisfied-component/video-testimonial/testimonial.html')).text();
-  
+      this.onInit();
       return html;
+    }
+
+    onInit() {
+      setTimeout(() => {
+        this.listenButtonsClick();
+      }, 0)
+    }
+
+    listenButtonsClick() {
+      const component = document.getElementById('sat-test-buttons');
+      if (component == null) return;
+      component.addEventListener('click', (event) => {
+        // if(event.target)
+        console.log('event')
+        console.log(event)
+      });
     }
 
 
